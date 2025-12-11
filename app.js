336ed4407caee69260008ac11be3f7cdfd277b40
66xmqtt4xx-rgb <66xmqtt4xx@privaterelay.appleid.com>
2025-12-11 19:09:15 +0700

    Change image source extension from .png to .jpg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let flipped=false;
 function flip(){
   let img=document.getElementById('img');
   flipped=!flipped;
-  img.src= flipped ? 'Image_Asset_1.png':'Image_Asset.png';
+  img.src= flipped ? 'Image_Asset_1.jpg':'Image_Asset.jpg';
 }
 function openFull(){
   location.href='full.html';
